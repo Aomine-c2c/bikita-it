775ce12793bacf9aaea7bd1f4c6439e1c0633067
armut <armut@404-bureau.local>
2026-07-16 09:42:45 +0200

    fix: change api tsconfig target to ES2022

--- apps/api/src/network/network.controller.ts
+++ apps/api/src/network/network.controller.ts
@@ -1,7 +1,5 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { NetworkService } from './network.service';
-import { CreateNetworkDto } from './dto/create-network.dto';
-import { UpdateNetworkDto } from './dto/update-network.dto';
 
 @Controller('network')
 export class NetworkController {
