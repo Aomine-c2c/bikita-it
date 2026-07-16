0348fbbc5eea8a4903cd361ddb7143653b8e2fef
armut <armut@404-bureau.local>
2026-07-16 09:29:53 +0200

    fix: import PrismaModule in NetworkModule

--- apps/api/src/network/network.module.ts
+++ apps/api/src/network/network.module.ts
@@ -2,7 +2,10 @@ import { Module } from '@nestjs/common';
 import { NetworkService } from './network.service';
 import { NetworkController } from './network.controller';
 
+import { PrismaModule } from '../prisma/prisma.module';
+
 @Module({
+  imports: [PrismaModule],
   controllers: [NetworkController],
   providers: [NetworkService],
 })
