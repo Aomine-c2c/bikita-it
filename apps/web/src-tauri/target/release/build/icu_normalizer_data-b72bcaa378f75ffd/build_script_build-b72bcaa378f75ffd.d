c528436123cde5fa9abc84c72ed9e54bcc37e74c
armut <armut@404-bureau.local>
2026-07-17 11:37:28 +0200

    qw123

---
+++ apps/web/src-tauri/target/release/build/icu_normalizer_data-b72bcaa378f75ffd/build_script_build-b72bcaa378f75ffd.d
@@ -0,0 +1,5 @@
+C:\Users\armut\404\BikitaIT\apps\web\src-tauri\target\release\build\icu_normalizer_data-b72bcaa378f75ffd\build_script_build-b72bcaa378f75ffd.d: C:\Users\armut\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\icu_normalizer_data-2.2.0\build.rs
+
+C:\Users\armut\404\BikitaIT\apps\web\src-tauri\target\release\build\icu_normalizer_data-b72bcaa378f75ffd\build_script_build-b72bcaa378f75ffd.exe: C:\Users\armut\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\icu_normalizer_data-2.2.0\build.rs
+
+C:\Users\armut\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\icu_normalizer_data-2.2.0\build.rs:
---
+++ apps/web/src-tauri/target/release/deps/cfg_if-b3e324ec569b289c.d
@@ -0,0 +1,7 @@
+C:\Users\armut\404\BikitaIT\apps\web\src-tauri\target\release\deps\cfg_if-b3e324ec569b289c.d: C:\Users\armut\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
+
+C:\Users\armut\404\BikitaIT\apps\web\src-tauri\target\release\deps\libcfg_if-b3e324ec569b289c.rlib: C:\Users\armut\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
+
+C:\Users\armut\404\BikitaIT\apps\web\src-tauri\target\release\deps\libcfg_if-b3e324ec569b289c.rmeta: C:\Users\armut\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
+
+C:\Users\armut\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs:
